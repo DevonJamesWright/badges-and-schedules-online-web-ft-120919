0c799b568f8570bc143bdbfaed63602b8d14035b
Devon James Wright <devonjamess.wright@gmail.com>
2019-12-10 22:33:47 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,4 +1,5 @@
 def badge_maker (name)
-  name = []
-  puts "Hello, my name is #{name}"
+  name = ["Arel", "Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  name.each { |name| puts "Hello, my name is #{name}"}
+  
 end
